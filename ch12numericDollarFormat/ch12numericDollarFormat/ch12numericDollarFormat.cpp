e5bab1969be3917ad1dfaef37a3e0e21f173e582
Kevin Bell <bellKevin@pm.me>
2022-10-24 14:07:10 -0600

    Update ch12numericDollarFormat.cpp

--- ch12numericDollarFormat/ch12numericDollarFormat/ch12numericDollarFormat.cpp
+++ ch12numericDollarFormat/ch12numericDollarFormat/ch12numericDollarFormat.cpp
@@ -2,25 +2,32 @@
 //
 
 #include <iostream>
+#include <iomanip>
+#include <sstream>
 #include <string>
 using namespace std;
 
-string dollarFormat(string); // function prototype
+// Function prototypes
+string dollarFormat(string);
+string dollarFormat(double);
 
 int main()
 {
     cout << "Chapter 12 Dollar Formatter by Kevin Bell\n\n";
-	string input; // user input
-	
-	//get the dollar amount from the user
-	cout << "Enter a dollar amount in the form of xxxxx.xx: ";
-	cin >> input;
+	double input = 76543.21;
+
+	cout << fixed << showpoint << setprecision(2);
+	cout << "Original value: " << input << endl;
+	//cout << "Enter a dollar amount in the form of nnnnn.nn: ";
+	//cin >> input;
 
 	//display the formatted dollar amount
 	cout << "Formatted amount: " << dollarFormat(input) << endl;
 	
+	system("pause");
 	return 0;
 } // end main
+
 
 //**************************************************************
 // Returns a $-formatted version of the input string.          *
@@ -30,7 +37,7 @@ string dollarFormat(string original)
 	string formatted = original; // formatted string to return
 	int decimalPos = formatted.find('.'); // position of decimal
 	int pos = decimalPos; // position to insert commas
-	
+
 	while (pos > 3) // insert commas until pos is less than 3
 	{
 		pos -= 3; // move pos back 3 spaces
@@ -38,4 +45,23 @@ string dollarFormat(string original)
 	} // end while loop
 	formatted.insert(0, "$"); // insert dollar sign
 	return formatted; // return formatted string
+} // end dollarFormat
+
+
+//**************************************************************
+// Returns a $-formatted version of the input double.          *
+//**************************************************************
+string dollarFormat(double value) {
+
+	//creat ostringstream oject
+	ostringstream ostr;
+	
+	//set format flags
+	ostr << fixed << setprecision(2);
+	
+	//insert value into stream
+	ostr << value;
+	
+	//return formatted string
+	return dollarFormat(ostr.str());
 } // end dollarFormat
